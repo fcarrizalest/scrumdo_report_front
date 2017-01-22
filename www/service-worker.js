25a3e2fd0c504dfed85335851af1a9aa2ae79695
Francisco Carrizales <fcarrizalest@gmail.com>
2017-01-21 22:18:29 -0600

    probelams con el cache

--- www/service-worker.js
+++ www/service-worker.js
@@ -6,8 +6,10 @@ importScripts('js/lib/sw-toolbox.js');
 var CACHE_NAME = 'dependencies-cache';
 var REQUIRED_FILES = [
   '/',
-  'index.html'
-
+  'index.html',
+  '/css/main.css',
+  '/js/common.js',
+  '/js/lib/require.js'
   //'manifest.json'
 
 
@@ -22,6 +24,8 @@ toolbox.precache(REQUIRED_FILES);
 toolbox.router.get('/', toolbox.fastest);
 toolbox.router.get('/:index.html', toolbox.fastest);
 toolbox.router.get('/js/lib/:file.js', toolbox.fastest);
+toolbox.router.get('/js/:lib.js', toolbox.fastest);
+toolbox.router.get('/css/main.css', toolbox.fastest);
 
 toolbox.router.get('/(.*)', toolbox.fastest,{origin: 'https://a5429.herokuapp.com'});
 
